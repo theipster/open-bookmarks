a126e6ad39d529c9f62153ca767236989388dc9d
Alan Ip <theipster@users.noreply.github.com>
2019-12-18 21:42:30 +0000

    Squashed 'vendor/BurntSushi/erd/' content from commit ed054c4
    git-subtree-dir: vendor/BurntSushi/erd
    git-subtree-split: ed054c4d50afd6740c8a3a49d1f7abb9f1e309d8

---
+++ app/Main.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+  (main)
+where
+
+import           Control.Applicative                 ((<|>))
+import           Control.Monad                       (forM_, guard)
+import qualified Data.ByteString                     as SB
+import           Data.Maybe                          (fromJust, fromMaybe)
+import qualified Data.Text.Lazy                      as L
+import           System.Exit                         (exitFailure)
+import           System.IO                           (hClose, hPutStrLn, stderr)
+
+import           Data.GraphViz
+import qualified Data.GraphViz.Attributes.Colors.X11 as C
+import qualified Data.GraphViz.Attributes.Complete   as A
+import qualified Data.GraphViz.Attributes.HTML       as H
+import           Data.GraphViz.Commands              (isGraphvizInstalled)
+import qualified Data.GraphViz.Types.Generalised     as G
+import           Data.GraphViz.Types.Monadic
+
+import           Erd.Config
+import           Erd.ER
+import           Erd.Parse
+import           Erd.Render                          (htmlAttr, htmlFont,
+                                                      withLabelFmt)
+
+main :: IO ()
+main = do
+  checkRequirements -- application may terminate here
+  conf <- configIO
+  er' <- uncurry loadER (cin conf)
+  case er' of
+    Left err -> do
+      hPutStrLn stderr err
+      exitFailure
+    Right er -> let erDot = dotER conf er
+                    toFile h = SB.hGetContents h >>= SB.hPut (snd $ cout conf)
+                    fmt = fromMaybe Pdf (outfmt conf)
+                 in graphvizWithHandle Dot erDot fmt toFile
+  hClose (snd $ cin conf)
+  hClose (snd $ cout conf)
+
+-- | Converts an entire ER-diagram from an ER file into a GraphViz graph.
+dotER :: Config -> ER -> G.DotGraph L.Text
+dotER conf er = graph' $ do
+  graphAttrs (graphTitle $ title er)
+  graphAttrs [ A.RankDir A.FromLeft
+             , A.Splines $ fromMaybe (fromJust . edgeType $ defaultConfig) (edgeType conf)
+             ]
+  nodeAttrs [shape PlainText] -- recommended for HTML labels
+  edgeAttrs [ A.Color [A.toWC $ A.toColor C.Gray50] -- easier to read labels
+            , A.MinLen 2 -- give some breathing room
+            , A.Style [A.SItem A.Dashed []] -- easier to read labels, maybe?
+            ]
+  forM_ (entities er) $ \e ->
+    node (name e) [toLabel (htmlEntity e)]
+  forM_ (rels er) $ \r -> do
+    let optss    = roptions r
+        rlab     = A.HtmlLabel . H.Text . htmlFont optss . L.pack . show
+        (l1, l2) = (A.TailLabel $ rlab $ card1 r, A.HeadLabel $ rlab $ card2 r)
+        label    = A.Label $ A.HtmlLabel $ H.Text $ withLabelFmt " %s " optss []
+    edge (entity1 r) (entity2 r) [label, l1, l2]
+
+-- | Converts a single entity to an HTML label.
+htmlEntity :: Entity -> H.Label
+htmlEntity e = H.Table H.HTable
+                 { H.tableFontAttrs = Just $ optionsTo optToFont $ eoptions e
+                 , H.tableAttrs = optionsTo optToHtml (eoptions e)
+                 , H.tableRows = rows
+                 }
+  where rows = headerRow : map htmlAttr (attribs e)
+        headerRow = H.Cells [H.LabelCell [] $ H.Text text]
+        text = withLabelFmt " [%s]" (hoptions e) $ boldFont hname
+        hname = htmlFont (hoptions e) (name e)
+        boldFont s = [H.Format H.Bold s]
+
+-- | Extracts and formats a graph title from the options given.
+-- The options should be title options from an ER value.
+-- If a title does not exist, an empty list is returned and `graphAttrs attrs`
+-- should be a no-op.
+graphTitle :: Options -> [A.Attribute]
+graphTitle topts =
+  let glabel = optionsTo optToLabel topts
+  in if null glabel then [] else
+       [ A.LabelJust A.JLeft
+       , A.LabelLoc A.VTop
+       , A.Label $ A.HtmlLabel $ H.Text $ htmlFont topts (head glabel)
+       ]
+
+checkRequirements :: IO ()
+checkRequirements = (isGraphvizInstalled >>= guard) <|> quitWithoutGraphviz msg
+  where
+    msg = "GraphViz is not installed on your system.\n" ++
+          "Please install it first, https://github.com/BurntSushi/erd"
---
+++ src/Erd/Config.hs
@@ -0,0 +1,239 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Erd.Config
+  ( Config(..)
+  , configIO
+  , defaultConfig
+  , defaultConfigFile
+  )
+where
+
+import           Control.Exception                 (tryJust)
+import           Control.Monad                     (guard)
+import qualified Data.ByteString.Char8             as B
+import           Data.Char                         (isSpace)
+import qualified Data.GraphViz.Attributes.Complete as A
+import qualified Data.GraphViz.Commands            as G
+import           Data.List                         (dropWhileEnd, intercalate, intersperse, concat)
+import qualified Data.Map                          as M
+import           Data.Maybe                        (isNothing)
+import           Data.Yaml                         (FromJSON (..), (.:))
+import qualified Data.Yaml                         as Y
+import qualified System.Console.GetOpt             as O
+import           System.Directory                  (getHomeDirectory)
+import           System.Environment                (getArgs)
+import           System.Exit                       (exitFailure)
+import           System.FilePath                   ((</>))
+import           System.IO                         (Handle, IOMode (..),
+                                                    openFile, stderr, stdin,
+                                                    stdout)
+import           System.IO.Error                   (isDoesNotExistError)
+import           Text.Printf                       (HPrintfType, hPrintf,
+                                                    printf)
+import           Text.RawString.QQ
+
+-- | Config represents all information from command line flags.
+data Config =
+  Config { cin        :: (String, Handle)
+         , cout       :: (String, Handle)
+         , outfmt     :: Maybe G.GraphvizOutput
+         , edgeType   :: Maybe A.EdgeType
+         , configFile :: Maybe FilePath
+         }
+
+-- | Represents fields that are stored in the configuration file.
+data ConfigFile = ConfigFile
+  { cFmtOut   :: String
+  , cEdgeType :: String
+  } deriving Show
+
+instance FromJSON ConfigFile where
+  parseJSON (Y.Object v) =
+    ConfigFile <$>
+    v .: "output-format" <*>
+    v .: "edge-style"
+  parseJSON _ = fail "Incorrect configuration file."
+
+defaultConfig :: Config
+defaultConfig =
+  Config { cin = ("<stdin>", stdin)
+         , cout = ("<stdout>", stdout)
+         , outfmt = Nothing
+         , edgeType = Just A.SplineEdges
+         , configFile = Nothing
+         }
+
+defaultConfigFile :: B.ByteString
+defaultConfigFile = B.unlines
+  [[r|# Erd (~/.erd.yaml) default configuration file.|],
+   B.append [r|output-format: pdf           # Supported formats: |] (defVals fmts),
+   B.append [r|edge-style: spline           # Supported values : |] (defVals edges)]
+  where
+    defVals = B.pack . concat . intersperse " " . M.keys
+
+-- | Creates a new Config value from command line options.
+-- If an output path is given and `--fmt` is omitted, then a format
+-- will be inferred from the output path extension.
+-- Failing all of that, PDF is used by default.
+configIO :: IO Config
+configIO = do
+  args <- getArgs
+  case O.getOpt O.Permute opts args of
+    (flags, [], []) -> do
+      conf <- foldl (\c app -> app c) (return defaultConfig) flags
+      let outpath = fst (cout conf)
+      return $
+        if isNothing (outfmt conf) && outpath /= "<stdout>" then
+          conf { outfmt = toGraphFmt $ takeExtension outpath }
+        else
+          conf
+    (_, _, errs@(_:_)) -> do
+      ef "Error(s) parsing flags:\n\t%s\n" $
+        intercalate "\n\t" $ map strip errs
+      exitFailure
+    (_, _, []) -> do
+      ef "erd does not have any positional arguments.\n\n"
+      usageExit
+
+-- | Order of processing command-line options is important to keep priority of
+-- configuration sources, in terms of increasing precedence.
+-- 1. Command-line options
+-- 2. Configuration file of arbitrary path, e.g. '-c"/tmp/myconfig.yaml"'
+-- 3. Configuration file: ~/.erd.yaml
+opts :: [O.OptDescr (IO Config -> IO Config)]
+opts =
+  [ O.Option "c" ["config"]
+      (O.OptArg (\mf cIO -> cIO >>= \c -> do
+                      globConfFile <- readGlobalConfigFile
+                      f <- readConfigFile mf
+                      case (f, globConfFile) of
+                        (Nothing, Nothing) ->      -- Config-file is desired, but unavailable.
+                          B.putStr defaultConfigFile >> return c
+                        (Nothing, Just globalC) -> -- Use global config-file from ~/.erd.yaml .
+                          return c {outfmt   = (toGraphFmt $ cFmtOut globalC),
+                                    edgeType = (toEdgeG $ cEdgeType globalC)}
+                        (Just localC, _) ->        -- Use user specified config-file.
+                          return c {outfmt   = (toGraphFmt $ cFmtOut localC),
+                                    edgeType = (toEdgeG $ cEdgeType localC)}
+                    ) "FILE")
+      "Configuration file."
+  , O.Option "i" ["input"]
+      (O.ReqArg (\fpath cIO -> do
+                   c <- cIO
+                   i <- openFile fpath ReadMode
+                   return $ c {cin = (fpath, i)}
+                )
+                "FILE")
+      ("When set, input will be read from the given file.\n"
+       ++ "Otherwise, stdin will be used.")
+  , O.Option "o" ["output"]
+      (O.ReqArg (\fpath cIO -> do
+                    c <- cIO
+                    o <- openFile fpath WriteMode
+                    return $ c {cout = (fpath, o)}
+                )
+                "FILE")
+      ("When set, output will be written to the given file.\n"
+       ++ "Otherwise, stdout will be used.\n"
+       ++ "If given and if --fmt is omitted, then the format will be\n"
+       ++ "guessed from the file extension.")
+  , O.Option "h" ["help"]
+      (O.NoArg $ const usageExit)
+      "Show this usage message."
+  , O.Option "f" ["fmt"]
+      (O.ReqArg (\fmt cIO -> do
+                    c <- cIO
+                    let mfmt = toGraphFmt fmt
+                    case mfmt of
+                      Nothing -> do
+                        ef "'%s' is not a valid output format." fmt
+                        exitFailure
+                      Just gfmt -> return c {outfmt = Just gfmt}
+                )
+                "FMT")
+      (printf "Force the output format to one of:\n%s"
+              (intercalate ", " $ M.keys fmts))
+  , O.Option "e" ["edge"]
+      (O.ReqArg (\edge cIO -> do
+                    c <- cIO
+                    let edgeG = toEdgeG edge
+                    case edgeG of
+                      Nothing -> do
+                        ef "'%s' is not a valid type of edge." edge
+                        exitFailure
+                      Just x -> return c {edgeType = Just x}
+                )
+                "EDGE")
+      (printf "Select one type of edge:\n%s"
+              (intercalate ", " $ M.keys edges))
+  ]
+
+-- | Reads and parses configuration file at default location: ~/.erd.yaml
+readGlobalConfigFile :: IO (Maybe ConfigFile)
+readGlobalConfigFile = do
+  mHome <- tryJust (guard . isDoesNotExistError) getHomeDirectory
+  case mHome of
+    Left _     -> return Nothing
+    Right home -> readConfigFile $ Just (home </> ".erd.yaml")
+
+-- | Reads and parses a configuration file, exceptions may come via
+-- AesonException.
+readConfigFile :: Maybe FilePath -> IO (Maybe ConfigFile)
+readConfigFile Nothing = return Nothing
+readConfigFile (Just f) = do
+  mHome <- tryJust (guard . isDoesNotExistError) $ B.readFile f
+  case mHome of
+    Left _     -> return Nothing
+    Right home -> Y.decodeThrow home
+
+-- | A subset of formats supported from GraphViz.
+fmts :: M.Map String (Maybe G.GraphvizOutput)
+fmts = M.fromList
+  [ ("pdf", Just G.Pdf)
+  , ("svg", Just G.Svg)
+  , ("eps", Just G.Eps)
+  , ("bmp", Just G.Bmp)
+  , ("jpg", Just G.Jpeg)
+  , ("png", Just G.Png)
+  , ("gif", Just G.Gif)
+  , ("tiff", Just G.Tiff)
+  , ("dot", Just G.Canon)
+  , ("ps",  Just G.Ps)
+  , ("ps2", Just G.Ps2)
+  , ("plain", Just G.Plain)
+  ]
+
+edges :: M.Map String (Maybe A.EdgeType)
+edges = M.fromList
+  [ ("spline", Just A.SplineEdges)
+  , ("ortho", Just A.Ortho)
+  , ("noedge", Just A.NoEdges)
+  , ("poly", Just A.PolyLine)
+  , ("compound", Just A.CompoundEdge)
+  ]
+
+-- | takeExtension returns the last extension from a file path, or the
+-- empty string if no extension was found. e.g., the extension of
+-- "wat.pdf" is "pdf".
+takeExtension :: String -> String
+takeExtension s = if null rest then "" else reverse ext
+  where (ext, rest) = span (/= '.') $ reverse s
+
+toGraphFmt :: String -> Maybe G.GraphvizOutput
+toGraphFmt ext = M.findWithDefault Nothing ext fmts
+
+toEdgeG :: String -> Maybe A.EdgeType
+toEdgeG edge = M.findWithDefault Nothing edge edges
+
+usageExit :: IO a
+usageExit = usage >> exitFailure
+
+usage :: IO ()
+usage = ef "%s\n" $ O.usageInfo "Usage: erd [flags]" opts
+
+ef :: HPrintfType r => String -> r
+ef = hPrintf stderr
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace

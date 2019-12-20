0db3c93a529add04ecdcabe5880a0e01d4939827
Alan Ip <theipster@users.noreply.github.com>
2019-12-20 01:44:54 +0000

    Bookmarks: Spring Initializr commit.

---
+++ services/bookmarks/src/test/java/com/github/theipster/bookmarks/BookmarksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theipster.bookmarks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

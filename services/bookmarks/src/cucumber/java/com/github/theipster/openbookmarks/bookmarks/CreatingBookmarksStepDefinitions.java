0e03d64f53f52e8291d64530b2ab72edb2f6521e
Alan Ip <theipster@users.noreply.github.com>
2020-01-02 02:59:39 +0000

    Bookmarks: implemented step defs (minus spinning up the service).
    Removed Spring Boot Gradle plugin, as it was causing conflicts with RestAssured
    v4.1.2. See https://stackoverflow.com/a/56556257 for more details.

--- services/bookmarks/src/cucumber/java/com/github/theipster/openbookmarks/bookmarks/CreatingBookmarksStepDefinitions.java
+++ services/bookmarks/src/cucumber/java/com/github/theipster/openbookmarks/bookmarks/CreatingBookmarksStepDefinitions.java
@@ -1,51 +1,91 @@
 package com.github.theipster.openbookmarks.bookmarks;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import cucumber.api.PendingException;	// io.cucumber:cucumber-java:4.*
-import java.net.URL;
-import java.util.Optional;
+import io.restassured.response.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CreatingBookmarksStepDefinitions {
 
+    private static final String UUID_REGEX = "[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}";
+
+    private String serviceHost;
+    private Response response;
+
+    @Before
+    public void initializeScenario() {
+        serviceHost = null;
+        response = null;
+    }
+
     @Given("I can access the bookmarks service")
     public void i_can_access_the_bookmarks_service() throws Throwable {
-	throw new PendingException();
+        serviceHost = "http://bookmarks-service:80/";   // @todo Load real service
     }
 
-    @Given("there is no existing bookmark of URL {url} and title {string}")
-    public void there_is_no_existing_bookmark_of_URL_url_and_title_title(Optional<URL> url, String title) throws Throwable {
-	throw new PendingException();
+    @Given("there is no existing bookmark of URL {string} and title {string}")
+    public void there_is_no_existing_bookmark_of_URL_url_and_title_title(String url, String title) throws Throwable {
+        // Don't create it. :)
+        // @todo Make this idempotent by finding and deleting any existing bookmarks.
     }
 
-    @When("I attempt to create a bookmark of URL {url} and title {string}")
-    public void i_attempt_to_create_a_bookmark_of_URL_url_and_title_title(Optional<URL> url, String title) throws Throwable {
-	throw new PendingException();
+    @When("I attempt to create a bookmark of URL {string} and title {string}")
+    public void i_attempt_to_create_a_bookmark_of_URL_url_and_title_title(String url, String title) throws Throwable {
+        response = new HttpRequest()
+            .setBaseUri(serviceHost)
+            .setBody(String.format("{\"url\": \"%s\", \"title\": \"%s\"}", url, title))
+            .post("/bookmarks");
     }
 
     @Then("I am told the bookmark was successfully created")
     public void i_am_told_the_bookmark_was_successfully_created() throws Throwable {
-	throw new PendingException();
+        assertEquals(201, response.getStatusCode());
     }
 
     @Then("I am given a link to the bookmark")
     public void i_am_given_a_link_to_the_bookmark() throws Throwable {
-	throw new PendingException();
+        String location = response.getHeader("Location");
+        assertTrue(
+            String.format("Location header %s does not match pattern /bookmarks/{uuid}.", location),
+            location.matches("/bookmarks/" + UUID_REGEX)
+        );
     }
 
-    @Given("there is an existing bookmark of URL {url} and title {string}")
-    public void there_is_an_existing_bookmark_of_URL_url_and_title_title(Optional<URL> url, String title) throws Throwable {
-	throw new PendingException();
+    @Given("there is an existing bookmark of URL {string} and title {string}")
+    public void there_is_an_existing_bookmark_of_URL_url_and_title_title(String url, String title) throws Throwable {
+        new HttpRequest()
+            .setBaseUri(serviceHost)
+            .setBody(String.format("{\"url\": \"%s\", \"title\": \"%s\"}", url, title))
+            .post("/bookmarks");
     }
 
     @Then("I am told the bookmark exists")
     public void i_am_told_the_bookmark_exists() throws Throwable {
-	throw new PendingException();
+        assertEquals(303, response.getStatusCode());
     }
 
     @Then("I am told the bookmark details are invalid")
     public void i_am_told_the_bookmark_details_are_invalid() throws Throwable {
-	throw new PendingException();
+        assertEquals(400, response.getStatusCode());
+    }
+
+    @When("I attempt to create a bookmark of title {string}")
+    public void i_attempt_to_create_a_bookmark_of_title_title(String title) throws Throwable {
+        response = new HttpRequest()
+            .setBaseUri(serviceHost)
+            .setBody(String.format("{\"title\": \"%s\"}", title))
+            .post("/bookmarks");
+    }
+
+    @When("I attempt to create a bookmark of URL {string}")
+    public void i_attempt_to_create_a_bookmark_of_URL_url(String url) throws Throwable {
+        response = new HttpRequest()
+            .setBaseUri(serviceHost)
+            .setBody(String.format("{\"url\": \"%s\"}", url))
+            .post("/bookmarks");
     }
 }
--- services/bookmarks/src/cucumber/resources/features/CreatingBookmarks.feature
+++ services/bookmarks/src/cucumber/resources/features/CreatingBookmarks.feature
@@ -15,7 +15,6 @@ Feature: Creating bookmarks
     Examples:
       |                url                 |       title       |
       |     'https://www.google.com/'      | 'Google homepage' |
-      |       'https://github.com/'        | 'Github homepage' |
       | 'https://github.com/fluidicon.png' | 'Github PNG logo' |
       |      'https://www.bbc.co.uk/'      |        ''         |
 
@@ -26,13 +25,10 @@ Feature: Creating bookmarks
     And I am given a link to the bookmark
 
     Examples:
-      |                url                 |       title       |
-      |     'https://www.google.com/'      | 'Google homepage' |
-      |       'https://github.com/'        | 'Github homepage' |
-      | 'https://github.com/fluidicon.png' | 'Github PNG logo' |
-      |      'https://www.bbc.co.uk/'      |        ''         |
+      |          url          |       title       |
+      | 'https://github.com/' | 'Github homepage' |
 
-  Scenario Outline: Creating an invalid bookmark
+  Scenario Outline: Creating a bookmark with an invalid URL
     When I attempt to create a bookmark of URL <url> and title <title>
     Then I am told the bookmark details are invalid
 
@@ -40,3 +36,19 @@ Feature: Creating bookmarks
       |     url     |       title       |
       |     ''      | 'Google homepage' |
       | 'not a url' | 'Github homepage' |
+
+  Scenario Outline: Creating a bookmark with a missing URL
+    When I attempt to create a bookmark of title <title>
+    Then I am told the bookmark details are invalid
+
+    Examples:
+      |       title       |
+      | 'Google homepage' |
+
+  Scenario Outline: Creating a bookmark with a missing title
+    When I attempt to create a bookmark of URL <url>
+    Then I am told the bookmark details are invalid
+
+    Examples:
+      |            url            |
+      | 'https://www.google.com/' |

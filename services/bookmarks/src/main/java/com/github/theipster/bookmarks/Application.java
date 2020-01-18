e90ea0e289cce85be99cd9a8cbea1ac1f9c91e5f
Alan Ip <theipster@users.noreply.github.com>
2020-01-18 20:14:31 +0000

    Bookmarks: rename BookmarksApplication to Application.

--- services/bookmarks/src/main/java/com/github/theipster/bookmarks/Application.java
+++ services/bookmarks/src/main/java/com/github/theipster/bookmarks/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookmarksApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmarksApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
